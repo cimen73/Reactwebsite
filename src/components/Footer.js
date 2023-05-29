dd80ef7e23b50746d6d4c94f5ba412e7cbc5f7ad
cimen73 <cimenugur73@gmail.com>
2023-05-29 13:41:47 +0300

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+function Footer() {
+  return (
+    <div className='footer'>
+       
+            <p> &copy; 2023 burger23.com</p>
+           
+       
+    </div>
+  )
+}
+
+export default Footer
